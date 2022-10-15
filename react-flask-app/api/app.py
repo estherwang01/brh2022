bd7f6273b6bf3a135e475915c9d2c7f89bdee024
Jerry Xu <28634832+jjjxu@users.noreply.github.com>
2022-10-15 13:52:30 -0400

    More fleshed out data?

--- react-flask-app/api/app.py
+++ react-flask-app/api/app.py
@@ -1,3 +1,4 @@
+import json
 import time
 import os
 import pandas as pd
@@ -17,13 +18,20 @@ zipcodes.set_index("zip", inplace=True)
 zipcodes = zipcodes["Subregion 1"].to_dict()
 
 
-def compute_co2(battery_size, zipcode):
+def compute_co2(battery_size: float, zipcode: int, num_charges: int = 1) -> dict:
 	global locales, zipcodes
-	total_wH_per_charge = battery_size * 2
-	power_grid_name = zipcodes[zipcode]
-	power_grid_co2 = locales[power_grid_name]["SRCO2RTA"]
-	total_co2 = (total_wH_per_charge / 1000) * power_grid_co2
-	return total_co2
+	watt_hour_charge = battery_size * 2
+	multiplier = watt_hour_charge * num_charges / 1000000 * 365
+	power_grid_name = zipcodes.get(zipcode, "Unknown")
+	if power_grid_name == "Unknown":
+		return "Unknown"
+	power_grid = locales[power_grid_name]
+	for key in power_grid:
+		power_grid[key] *= multiplier
+	return power_grid
+
+
+# compute_co2(100.0, 1886, 1)
 
 
 @app.route('/time')
@@ -35,8 +43,37 @@ def get_current_time():
 def get_zipcode():
 	return {'region': zipcodes.get(14850, "Unknown")}
 
+
+@app.route('/check_zip/<int:zipcode>', methods=['GET'])
+def check_zip(zipcode: int):
+	return {'region': zipcodes.get(zipcode, "Unknown")}
+
+
 @app.route('/compute_co2', methods=['POST'])
 def get_co2():
-    battery_size = request.json['battery_size']
-    zipcode = request.json['zipcode']
-    return {'co2' : compute_co2(battery_size, zipcode)}
+	body = json.loads(request.data)
+	zipcode = int(request.json['zipcode'])
+	ret_val = {"carbondioxide": 0, "methane": 0, "nitrogen": 0, "sulfur": 0}
+	for device in body["devices"]:
+		battery_size = 0
+		num_charges = device["charges_per_day"]
+		if device["mode"] == "normal":
+			if device["device_class"] == "laptop":
+				if device["device_size"] == "small":
+					battery_size = 60.0
+				elif device["device_size"] == "large":
+					battery_size = 100.0
+			elif device["device_class"] == "tablet":
+				battery_size = 25.0
+			elif device["device_class"] == "phone":
+				battery_size = 12.0
+		elif device["mode"] == "advanced":
+			battery_size = device["device_battery_size_wh"]
+		computed_stats = compute_co2(battery_size, zipcode, num_charges)
+		ret_val["carbondioxide"] += computed_stats["SRCO2RTA"]
+		ret_val["methane"] += computed_stats["SRCH4RTA"]
+		ret_val["nitrogen"] += computed_stats["SRN2ORTA"] + computed_stats["SRNOXRTA"]
+	ret_val["spacex_launches"] = ret_val["carbondioxide"] / 300.0 / 2000
+	ret_val["annual_cow"] = ret_val["methane"] / 220.0
+	ret_val["miles_cheating_volkswagen"] = ret_val["nitrogen"] / 2.2 * 1000 / 1.6 / 1.5
+	ret_val["miles_honest_volkswagen"] = ret_val["nitrogen"] / 2.2 * 1000 / 1.6 / 0.043
