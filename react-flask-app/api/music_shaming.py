07cb28926598eb2d3bd6fc79e1bf715e82a5ae3c
Jackson Subcleff <jts324@cornell.edu>
2022-10-15 16:24:22 -0400

    music shaming

---
+++ react-flask-app/api/music_shaming.py
@@ -0,0 +1,19 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+playlist_id_lookup = None
+
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+def get_playlist_artists(playlist_id):
+    artists = []
+    tracks = sp.playlist(playlist_id)['tracks']['items']
+    for item in tracks:
+        for artist in item['track']['artists']:
+            if artist['name'] not in artists: artists.append(artist['name'])
+    return artists
+
+artists = get_playlist_artists("1EbsEZUl2jYURerJERuDqA")
+print(artists)
