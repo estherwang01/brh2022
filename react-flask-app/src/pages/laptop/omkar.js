3bc69ba200bbc3182258edb5c1dd3864c9fe44f1
Esther Wang <ew375@cornell.edu>
2022-10-15 18:19:58 -0400

    omkar

--- react-flask-app/src/pages/laptop/omkar.js
+++ react-flask-app/src/pages/laptop/omkar.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
+import train from "../../files/train.jpg"
 
 function Omkar() {
   const [view, setView] = useState(0); 
 
   return (
-    <div className="App">
-        Cube cube cube cube cube
+    <div className="App" >
+      {view == 0 ? 
+      <div style={{position: "absolute", top: "20%", left: "50%", transform:"translate(-50%, 0)"}}>
+        <div style={{display: "flex",  width: "fit-content", margin: "auto"}}>
+          <div className='title2'>Enter what city you live in</div><input className='input'></input>
+        </div>
+        <button className='button' onClick={() => setView(1)}>lfg fucking go!</button>
+      </div>
+                   : 
+        <div>
+          <div className='title2'>Here's a customized plan to improve your public transportation:</div>
+          <img src = {train}/>
+          </div>}
     </div>
   );
 }
--- react-flask-app/src/pages/title/title.js
+++ react-flask-app/src/pages/title/title.js
@@ -20,9 +20,9 @@ function Title(props) {
 
     const [video, setVideo] = useState(0); 
     const strings = [
-        ["You're out of touch.", "Hi out of touch. I'm Jerry!", "Hey guess what? You're a Dipshit."], 
+        ["You're out of touch.", "Hi out of touch. I'm Jerry!", "Hey Jackson guess what? You're a Dipshit."], 
         ["Touching grass is sometimes enough.", "No amount of touching grass makes me less of an oblivious fuck", "Guess what I'm the dipshit"],
-        ["I just code.", "Call me Qlobal Po!nts!", "AAAAAAAAAAAAAAAAAAAAAAAAAAAA"]
+        ["I just code.", "Call me Qlobal Po!nts!", "AAAAAAAAAAAAAAAAAAAAAAAAAAAA", "At least I don't trade oil futures"]
     ]
 
     useEffect(()=>{
@@ -42,51 +42,14 @@ function Title(props) {
     <div className="Title" style={{display: "flex"}}>
         <img src={left} onClick={() => switchThing(-1)} className='but left'/>
         <div className="titleContainer">
-        {
-            video == 0 && 
-            <video autoPlay loop muted className='akuna'>
-            <source src={akuna} type="video/mp4"/>
-        </video>
-        }
-        {
-            video == 1 && 
-            <video autoPlay loop muted className='akuna'>
-            <source src={elephant} type="video/mp4"/>
-        </video>
-        }
-        {
-            video == 2 && 
-            <video autoPlay loop muted className='akuna'>
-            <source src={omkar} type="video/mp4"/>
-        </video>
-        }
-        <div  className='cover2'></div>
-        {
-            video == 0 &&         
-            <Typed
-            className='typing'
-                strings={strings[0]}
-                    typeSpeed={80} backSpeed={10}  loop
-            />
-        }
-        {
-            video == 1 &&         
-            <Typed
-            className='typing'
-                strings={strings[1]}
-                    typeSpeed={80} backSpeed={10}  loop
-            />
-        }
-                {
-            video == 2 &&         
-            <Typed
-            className='typing'
-                strings={strings[2]}
-                    typeSpeed={80} backSpeed={10}  loop
-            />
-        }
-
-        <button className={'button titleButton'} onClick={() => props.onClick(video + 1)}>TELL ME MORE!</button>
+            {video == 0 && <video autoPlay loop muted className='akuna'><source src={akuna} type="video/mp4"/></video>}
+            {video == 1 && <video autoPlay loop muted className='akuna'><source src={elephant} type="video/mp4"/></video>}
+            {video == 2 && <video autoPlay loop muted className='akuna'><source src={omkar} type="video/mp4"/></video>}
+            <div  className='cover2'></div>
+            {video == 0 && <Typed className='typing' strings={strings[0]} typeSpeed={80} backSpeed={10}  loop/>}
+            {video == 1 && <Typed className='typing' strings={strings[1]} typeSpeed={80} backSpeed={10}  loop/>}
+            {video == 2 && <Typed className='typing' strings={strings[2]} typeSpeed={80} backSpeed={10}  loop/>}
+            <button className={'button titleButton'} onClick={() => props.onClick(video + 1)}>TELL ME MORE!</button>
         </div>
         <img src={right} onClick={() => switchThing(1)} className='but right'/>
     </div>
