e86cbd220d34df94a3eced427139227ba747165f
Jerry Xu <28634832+jjjxu@users.noreply.github.com>
2022-10-15 14:03:34 -0400

    elon_musk

--- react-flask-app/api/app.py
+++ react-flask-app/api/app.py
@@ -12,6 +12,7 @@ zipcodes_path = os.path.join(data_path, r"zipcodes.csv")
 
 locales = pd.read_csv(locales_path, index_col=None)
 locales.set_index("SUBRGN", inplace=True)
+locales = locales.rename(columns=lambda x: x.strip())
 locales = locales.to_dict(orient="index")
 zipcodes = pd.read_csv(zipcodes_path, index_col=None)
 zipcodes.set_index("zip", inplace=True)
@@ -52,8 +53,9 @@ def check_zip(zipcode: int):
 @app.route('/compute_co2', methods=['POST'])
 def get_co2():
 	body = json.loads(request.data)
-	zipcode = int(request.json['zipcode'])
-	ret_val = {"carbondioxide": 0, "methane": 0, "nitrogen": 0, "sulfur": 0}
+	zipcode = body.get("zipcode", "0")
+	zipcode = int(zipcode)
+	ret_val = {"carbondioxide": 0, "methane": 0, "nitrogen": 0}
 	for device in body["devices"]:
 		battery_size = 0
 		num_charges = device["charges_per_day"]
@@ -70,10 +72,11 @@ def get_co2():
 		elif device["mode"] == "advanced":
 			battery_size = device["device_battery_size_wh"]
 		computed_stats = compute_co2(battery_size, zipcode, num_charges)
-		ret_val["carbondioxide"] += computed_stats["SRCO2RTA"]
-		ret_val["methane"] += computed_stats["SRCH4RTA"]
-		ret_val["nitrogen"] += computed_stats["SRN2ORTA"] + computed_stats["SRNOXRTA"]
+		ret_val["carbondioxide"] += computed_stats.get("SRCO2RTA", 0)
+		ret_val["methane"] += computed_stats.get("SRCH4RTA", 0)
+		ret_val["nitrogen"] += computed_stats.get("SRN2ORTA", 0) + computed_stats.get("SRNOXRTA", 0)
 	ret_val["spacex_launches"] = ret_val["carbondioxide"] / 300.0 / 2000
 	ret_val["annual_cow"] = ret_val["methane"] / 220.0
 	ret_val["miles_cheating_volkswagen"] = ret_val["nitrogen"] / 2.2 * 1000 / 1.6 / 1.5
 	ret_val["miles_honest_volkswagen"] = ret_val["nitrogen"] / 2.2 * 1000 / 1.6 / 0.043
+	return ret_val
